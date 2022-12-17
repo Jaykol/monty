e1fce7c63f51126ef70e9804cdeb3c7c9219b51b
jesutofunmi <jekola.ajekola@gmail.com>
2022-12-17 06:15:14 -0800

    monty

--- push.c
+++ push.c
@@ -1,25 +1,37 @@
 #include "monty.h"
 /**
- * f_pstr - prints the string starting at the top of the stack,
- * followed by a new
+ * f_push - add node to the stack
  * @head: stack head
  * @counter: line_number
  * Return: no return
 */
-void f_pstr(stack_t **head, unsigned int counter)
+void f_push(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
-	(void)counter;
+	int n, j = 0, flag = 0;
 
-	h = *head;
-	while (h)
+	if (bus.arg)
 	{
-		if (h->n > 127 || h->n <= 0)
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
 		{
-			break;
-		}
-		printf("%c", h->n);
-		h = h->next;
-	}
-	printf("\n");
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
